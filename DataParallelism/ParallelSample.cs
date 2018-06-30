70a7aa829b17c5a9ad8564499dc4f098c85d70a6
saad <msaadsaif5@gmail.com>
2018-07-01 00:20:09 +0300

    Folder hierarchy for TPL

---
+++ DataParallelism/ParallelSample.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DataParallelism
+{
+    static class ParallelSample
+    {
+        public static void RunParallelFor(int maximumParallelism = Int32.MaxValue, CancellationToken token = new CancellationToken(), TaskScheduler scheduler = null)
+        {
+            var options = new ParallelOptions { CancellationToken = token, MaxDegreeOfParallelism = maximumParallelism, TaskScheduler = scheduler };
+            var resut = Parallel.For(0, 1000, options, (i, loopState) =>
+            {
+                if (loopState.ShouldExitCurrentIteration || loopState.IsStopped || loopState.IsExceptional)
+                    return;
+
+                if (i == 500)
+                {
+                    loopState.Break();
+                }
+
+                i++;
+            });
+
+            Console.WriteLine($"Loop completion status:  {resut.IsCompleted}\nLoop's lowest break iteration: {resut.LowestBreakIteration}");
+        }
+
+        public static void RunParallelForEach()
+        {
+            // The sum of these elements is 40.
+            int[] input = { 4, 1, 6, 2, 9, 5, 10, 3 };
+            int sum = 0;
+
+            try
+            {
+                Parallel.ForEach(
+                        input,                          // source collection
+                        () => 0,                            // thread local initializer
+                        (n, loopState, localSum) =>     // body
+                        {
+                            localSum += n;
+                            Console.WriteLine("Thread={0}, n={1}, localSum={2}", Thread.CurrentThread.ManagedThreadId, n, localSum);
+                            return localSum;
+                        },
+                        (localSum) =>
+                        {
+                            Interlocked.Add(ref sum, localSum);
+                        }// thread local aggregator
+                    );
+
+                Console.WriteLine("\nSum={0}", sum);
+            }
+            // No exception is expected in this example, but if one is still thrown from a task,
+            // it will be wrapped in AggregateException and propagated to the main thread.
+            catch (AggregateException e)
+            {
+                Console.WriteLine("Parallel.ForEach has thrown an exception. THIS WAS NOT EXPECTED.\n{0}", e);
+            }
+        }
+
+        public static void RunParallelInvoke()
+        {
+            try
+            {
+                Parallel.Invoke(
+                    BasicAction,	// Param #0 - static method
+                    () =>			// Param #1 - lambda expression
+                    {
+                        Console.WriteLine("Method=beta, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+                    },
+                    delegate ()		// Param #2 - in-line delegate
+                    {
+                        Console.WriteLine("Method=gamma, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+                    }
+                );
+            }
+            // No exception is expected in this example, but if one is still thrown from a task,
+            // it will be wrapped in AggregateException and propagated to the main thread.
+            catch (AggregateException e)
+            {
+                Console.WriteLine("An action has thrown an exception. THIS WAS UNEXPECTED.\n{0}", e.InnerException.ToString());
+            }
+        }
+
+        static void BasicAction()
+        {
+            Console.WriteLine("Method=alpha, Thread={0}", Thread.CurrentThread.ManagedThreadId);
+        }
+    }
+}
---
+++ DataParallelism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DataParallelism
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Executing Parallel For Each\n");
+            ParallelSample.RunParallelForEach();
+
+            Console.WriteLine("\nExecuting Parallel For\n");
+            ParallelSample.RunParallelFor();
+
+            Console.WriteLine("\nExecuting Parallel Invoke\n");
+            ParallelSample.RunParallelInvoke();
+
+            Console.ReadLine();
+        }
+    }
+}
