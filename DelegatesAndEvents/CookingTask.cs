77e02587da0ab37495975dd2ec0ae7e47ba43f60
Saad <msaadsaif5@gmail.com>
2018-12-01 19:12:46 +0300

    delegate sample

---
+++ DelegatesAndEvents/CookingTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+
+namespace DelegatesAndEvents
+{
+    class CookingTask
+    {
+        public delegate string GetFavoriteDish(long personId);
+
+        GetFavoriteDish getFavDish;
+
+        public CookingTask(GetFavoriteDish getFavoriteDish)
+        {
+            getFavDish = getFavoriteDish;
+        }
+
+        public void CookForPerson(long personId)
+        {
+            string dish = getFavDish(personId);
+            Console.WriteLine("Cooking...");
+            Thread.Sleep(1000 * 5);
+            Console.WriteLine($"Cooked {dish} for {personId}");
+        }
+    }
+}
---
+++ DelegatesAndEvents/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace DelegatesAndEvents
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task = new CookingTask(GetFavoriteDish);
+            task.CookForPerson(1);
+            task.CookForPerson(2);
+            task.CookForPerson(9);
+
+            Console.ReadLine();
+        }
+
+        static string GetFavoriteDish(long personId)
+        {
+            switch (personId)
+            {
+                case 1:
+                    return "Noodles";
+                case 2:
+                    return "Biryani";
+                case 3:
+                    return "Fish";
+                default:
+                    return "Ice Cream";
+            }
+        }
+    }
+}
